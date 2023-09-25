6b2b4881d66b852db107ac813da2f35179f86d4b
Dubrov21 <vald5red@yandex.ru>
2023-09-25 20:11:16 +0300

    Добавьте файлы проекта.

---
+++ PrintEvenNumbers.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace PrintEvenNumbers
+{
+    public class Answer { 
+            static void PrintEvenNumbers(int number)
+        {
+            for (int i = 2; i <= number; i++)
+            {
+                bool isEven;
+                isEven = (i % 2 == 0) ? true : false;
+                if(isEven)
+                {
+                    Console.Write(" " + i);
+                }
+            }
+                        
+        }
+    
+        static public void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            int number;
+
+            if(args.Length >= 1)
+            {
+                number = int.Parse(args[0]);
+            }
+            else
+            {
+                number = 11;
+            }
+            PrintEvenNumbers(number);
+        }
+    }
+}
